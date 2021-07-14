7bc50585d53bf669a62f617c44c6e22e3fa6fe4d
swynnejr <saucersam@gmail.com>
2021-07-13 21:58:54 -0500

    needs tweaks, but mostly done

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+
+
+app = Flask(__name__)
+app.secret_key = 'the game is secure af'
+
+@app.route('/')
+def numberGuess():
+    if not 'secret' in session:
+        session['secret'] = random.randint(1,1000000)
+    return render_template("index.html")
+
+@app.route('/guess', methods = ['POST'])
+def guessChecker():
+    if session['secret'] > int(request.form['guess']):
+        session['result'] = "Too low"
+    elif session['secret'] < int(request.form['guess']):
+        session['result'] = "Too high"
+    else:
+        session['result'] = "You win!"
+    return redirect ('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
